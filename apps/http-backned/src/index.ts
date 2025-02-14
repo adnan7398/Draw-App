0a20270b0c9baa06d2a5a40618b0b86dd36de2a7
adnan7398 <123adnansiddiqui@gmail.com>
2025-02-14 15:20:01 +0530

    compeleted chat backend

--- apps/http-backned/src/index.ts
+++ apps/http-backned/src/index.ts
@@ -92,20 +92,20 @@ app.post("/room",Middleware,async(req,res)=>{
             message:"this room is already exist"
         })
     }
-
-    app.get("/chats/:roomId",async (req,res)=>{
-        const roomId = Number(req.params.roomId);
-        const messages  = await prismaClient.room.findMany({
-           where:{
-            id:roomId,
-           },orderBy:{
+})
+app.get("/chats/:roomId",async (req,res)=>{
+    const roomId = Number(req.params.roomId);
+    const messages  = await prismaClient.chat.findMany({
+       where:{
+        roomId:roomId,
+       },
+       orderBy:{
             id:"desc"
-           },
-           take:50
-        })
-        res.json({
-            messages
-        })
+       },
+       take:50
+    })
+    res.json({
+        messages
     })
 })
 app.listen(3002);
